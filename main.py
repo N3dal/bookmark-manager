88c83eaf7abcf19ea6feb86fcf3fa87b2f61336e
N3dal <nedalxzo@gmail.com>
2022-05-07 14:13:06 +0200

    create main_menu() function

--- main.py
+++ main.py
@@ -39,10 +39,36 @@ clear()
 
 # create the DEFAULTS.
 WEBSITES_DATA_FILE_NAME = "websites_data.json"
+PROGRAM_NAME = "Simple Bookmark Manager"
+PROGRAM_OPTIONS = (
+    "Add new website",
+    "Show all websites",
+    "Edit websites data",
+    "Show program Log",
+    "Quit"
+)
+
+
+def main_menu():
+    """draw the main menu that will have,
+    all the program options."""
+
+    # first wipe the terminal screen.
+    clear()
+
+    print(PROGRAM_NAME.center(80, '-'))
+
+    # print program options.
+    for option_number, option_name in enumerate(PROGRAM_OPTIONS, 1):
+        print(f"[{option_number}] {option_name}.".center(80))
+
+    # now get the user input and return it.
+
+    return input('\n'.ljust(40) + ': ').strip().lower()
 
 
 def main():
-    pass
+    main_menu()
 
 
 if __name__ == "__main__":
