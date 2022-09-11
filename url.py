953e3d48d0d41b39b473b13da687ccb87d4a448d
N3dal <nedalxzo@gmail.com>
2022-09-11 12:46:27 +0200

    create open_in_browser method

--- url.py
+++ url.py
@@ -1,10 +1,28 @@
 """
 Url type to deal with storing urls.
 """
+from webbrowser import open_new_tab
+from webbrowser import open_new as open_new_window
 
 
 class Url:
     """"""
 
-    def __init__(self):
-        pass
+    def __init__(self, url: str):
+        self.url = url
+        self.url_alias = ""
+
+    def open_in_browser(self, open_in_new_window=False):
+        """
+        open url using your default browser
+        return True if everything going fine other wise its return False
+        to open the url in new window change 'open_in_new_window' to True.
+        """
+
+        if open_in_new_window:
+            # open the url on new window.
+            open_in_new_window(self.url)
+
+        else:
+            # open the url in new tab.
+            open_new_tab(self.url)
